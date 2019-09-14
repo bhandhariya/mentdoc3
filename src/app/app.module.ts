280a116987c2886cf50d72fd73ce8234c4ab7051
bhandhariya <sainiraja59@gmail.com>
2019-09-14 15:24:23 +0530

    feature component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/shared/header/header.component';
 import { Slider1Component } from './home/shared/slider1/slider1.component';
+import { Feature1Component } from './home/shared/feature1/feature1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    Slider1Component
+    Slider1Component,
+    Feature1Component
   ],
   imports: [
     BrowserModule,
