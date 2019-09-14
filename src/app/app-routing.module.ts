280490b94f204e3b78a33963af8351b603f4b10a
bhandhariya <sainiraja59@gmail.com>
2019-09-14 16:03:47 +0530

    security routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SecurityComponent } from './security/security.component';
 
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'security',component:SecurityComponent},
 ];
 
 @NgModule({
