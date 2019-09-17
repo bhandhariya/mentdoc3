21c084cc76fe527644bb6d0cfaa9082d639fefa1
bhandhariya <sainiraja59@gmail.com>
2019-09-17 15:41:19 +0530

    backed added in same repo

---
+++ routes/courousal_router.js
@@ -0,0 +1,14 @@
+  
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/many', function(req,res){
+    console.log('working')
+});
+
+
+
+
+module.exports = router;
