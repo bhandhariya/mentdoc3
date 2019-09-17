27ac52e38baf22ec3cbc3938c42743320ff5f574
bhandhariya <sainiraja59@gmail.com>
2019-09-17 15:26:16 +0530

    courousal router created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+var CorousalRouter=require('./routes/courousal_router');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/mentdoc3'));
 
@@ -11,5 +11,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/mentdoc3/index.html'));
 });
 
+app.use('/cor',CorousalRouter)
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
