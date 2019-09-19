039da55df446db4b9ab882fcbd8cc3629d937b34
bhandhariya <sainiraja59@gmail.com>
2019-09-19 17:22:18 +0530

    header changed

--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -11,5 +11,16 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+  }
+  closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+  }
+  openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
 }
