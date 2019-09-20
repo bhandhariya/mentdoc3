519f975905126b60d16e0dc4ace7dda84ba9b16b
bhandhariya <sainiraja59@gmail.com>
2019-09-20 10:51:43 +0530

    20-sept first commit

--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
@@ -22,5 +23,11 @@ export class HeaderComponent implements OnInit {
   }
   closeRightMenu() {
     document.getElementById("rightMenu").style.display = "none";
+  }
+  gotoSecurity(){
+    this.router.navigateByUrl('security')
+  }
+  gotoAdmin(){
+    this.router.navigateByUrl('admin')
   }
 }
