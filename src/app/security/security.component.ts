3ab7d22ed817de266156942c16559c89b924c608
bhandhariya <sainiraja59@gmail.com>
2019-09-24 14:05:21 +0530

    security page completed

--- src/app/security/security.component.ts
+++ src/app/security/security.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
@@ -10,6 +10,22 @@ export class SecurityComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      $('.leftmenutrigger').on('click', function(e) {
+          $('.side-nav').toggleClass("open");
+          e.preventDefault();
+      });
+  });
+  $(function() {
+      $(document).scroll(function() {
+          var $nav = $(".fixed-top");
+          $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+  $(".card-link").click(function() {
+      $(this).toggleClass("down");
+  })
   }
+
 
 }
