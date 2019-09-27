73929d4dc38026f7fdc79c62f75f515176f0e15e
bhandhariya <sainiraja59@gmail.com>
2019-09-27 16:07:16 +0530

    new blog design added

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-
+arr=[
+  {id:1},
+  {id:2},
+  {id:3},
+  {id:4},
+  {id:5},
+  {id:6},
+  {id:7},
+  {id:8},
+  {id:9},
+];
   constructor() { }
 
   ngOnInit() {
