698b23eddc8b2650638a825aab9ee5366677dbea
bhandhariya <sainiraja59@gmail.com>
2019-09-27 13:21:13 +0530

    services page created

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -10,6 +10,16 @@ export class ServicesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+          e.preventDefault();
+          $(this).siblings('a.active').removeClass("active");
+          $(this).addClass("active");
+          var index = $(this).index();
+          $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+          $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+      });
+  });
   }
 
 }
